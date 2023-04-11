65d45276abcd446016bf9e84f677511b724575b8
hiep2001job <hiepdc1996n512@vlvh.ctu.edu.vn>
2023-04-11 14:34:54 +0700

    update profile

--- src/pages/info_profile/InfoProfile.jsx
+++ src/pages/info_profile/InfoProfile.jsx
@@ -57,6 +57,11 @@ import { useEffect, useLayoutEffect } from "react";
 // });
 
 const InfoProfile = () => {
+  const [activeTab, setActiveTab] = useState("1");
+  const tabChange = (tab) => {
+    if (activeTab !== tab) setActiveTab(tab);
+  };
+
   const dispatch = useDispatch();
   const userDetail = useUserDetail();
   const userProfile = useSelector((state) => state.auth.userProfile);
@@ -69,19 +74,19 @@ const InfoProfile = () => {
     mode: "onBlur",
   });
 
-  useLayoutEffect(() => {   
-
-    console.log("user",userDetail);
-    if(userDetail){
+  useLayoutEffect(() => {
+    console.log("user", userDetail);
+    if (userDetail) {
       reset({
         email: userDetail.email,
         address: userDetail.address,
         phone: userDetail.phone,
         image: userDetail.image,
         gender: userDetail.gender,
-    });
+        userfullname: userDetail.gender,
+      });
     }
-  }, [dispatch,userDetail]);
+  }, [dispatch, userDetail]);
 
   const profileOptions = {
     email: {
@@ -115,18 +120,14 @@ const InfoProfile = () => {
       required: "Address cannot be empty!",
     },
   };
-  const handleErrors=()=>{};
+  const handleErrors = () => {};
   const onSubmit = (data) => {
     const newData = { ...data, id: userProfile.id };
     console.log(newData);
     // dispatch(updateProfile(newData));
   };
 
-  const [activeTab, setActiveTab] = useState("1");
 
-  const tabChange = (tab) => {
-    if (activeTab !== tab) setActiveTab(tab);
-  };
 
   // handle change event of input file
   const onChangeFile = async (event) => {
@@ -145,6 +146,7 @@ const InfoProfile = () => {
         // dispatch(openSnackbar(snackBarPayload));
       });
   };
+  
 
   return (
     <React.Fragment>
@@ -223,10 +225,7 @@ const InfoProfile = () => {
                 <CardBody className="p-4">
                   <TabContent activeTab={activeTab}>
                     <TabPane tabId="1">
-                      <form onSubmit={handleSubmit(
-                              onSubmit,
-                              handleErrors
-                            )}>
+                      <form onSubmit={handleSubmit(onSubmit, handleErrors)}>
                         <Row>
                           <Col lg={6}>
                             <div className="mb-3">
@@ -269,7 +268,8 @@ const InfoProfile = () => {
                                   profileOptions.userfullname
                                 )}
                               />
-                              <p>{errors?.userfullname?.message}</p>
+                              
+                              <p className="text-red-500">{errors?.userfullname?.message}</p>
                             </div>
                           </Col>
 
@@ -284,12 +284,12 @@ const InfoProfile = () => {
                               <select
                                 className="form-select mb-3"
                                 name="gender"
-                                {...register("gender")}
+                                {...register("gender",profileOptions.gender)}
                               >
-                                <option>Select your gender </option>
-                                <option value="1">Male</option>
-                                <option value="2">Female</option>
-                                <option value="3">Others</option>
+                                <option >Select your gender </option>
+                                <option selected={userDetail.gender==1} value="1">Male</option>
+                                <option selected={userDetail.gender==2} value="2">Female</option>
+                                <option selected={userDetail.gender==3} value="3">Others</option>
                               </select>
                               <small className="text-red-500">
                                 {errors?.gender && errors.gender.message}
@@ -311,9 +311,9 @@ const InfoProfile = () => {
                                 id="phoneInput"
                                 placeholder="Enter your phone number"
                                 name="phone"
-                                {...register("phone")}
+                                {...register("phone",profileOptions.phone)}
                               />
-                              <p>{errors?.phone?.message}</p>
+                              <p className="text-red-500">{errors?.phone?.message}</p>
                             </div>
                           </Col>
 
@@ -331,9 +331,9 @@ const InfoProfile = () => {
                                 id="addressInput"
                                 placeholder="Enter your address"
                                 name="address"
-                                {...register("address")}
+                                {...register("address",profileOptions.address)}
                               />
-                              <p>{errors?.address?.message}</p>
+                              <p className="text-red-500">{errors?.address?.message}</p>
                             </div>
                           </Col>
 
@@ -510,95 +510,6 @@ const InfoProfile = () => {
       </div>
     </React.Fragment>
 
-    // <div className="flex justify-between">
-    //   <div className="pt-12 pl-20 pb-10 border w-70%">
-    //     <form>
-    //       <div className="mb-2">
-    //         <label className="font-semibold pr-1" htmlFor="">
-    //           Username:
-    //         </label>
-    //         <input type="text" value="David Passenger" />
-    //       </div>
-    //       <div className="mb-2">
-    //         <label className="font-semibold  pr-1" htmlFor="">
-    //           Email:
-    //         </label>
-    //         <input type="text" value="hminhnhut99@gmail.com" />
-    //       </div>
-    //       <div className="mb-2">
-    //         <label className="font-semibold  pr-1" htmlFor="">
-    //           Phone:
-    //         </label>
-    //         <input type="text" value="038 972 8084" />
-    //       </div>
-    //       <div className="mb-2">
-    //         <label className="font-semibold  pr-1" htmlFor="">
-    //           Address:
-    //         </label>
-    //         <input type="text" value="Can Tho" />
-    //       </div>
-    //       {/* gender  */}
-    //       <div className="w-full mb-2 relative">
-    //         <h4 className="font-semibold">Gender:</h4>
-    //         <div className="flex items-center">
-    //           <div className="flex items-center mr-2 text-sm [&>*]:cursor-pointer">
-    //             <input
-    //               id="men"
-    //               name="gender"
-    //               // {...register("gender", loginOptions.gender)}
-    //               type="radio"
-    //               value="0"
-    //               className="mr-1"
-    //             />
-    //             <label class htmlFor="men">
-    //               Men
-    //             </label>
-    //           </div>
-    //           <div className="flex items-center mr-2 text-sm [&>*]:cursor-pointer">
-    //             <input
-    //               type="radio"
-    //               id="women"
-    //               value="1"
-    //               className="mr-1"
-    //               name="gender"
-    //               // {...register("gender", loginOptions.gender)}
-    //             />
-    //             <label htmlFor="women">Women</label>
-    //           </div>
-    //           <div className="flex items-center mr-2 text-sm [&>*]:cursor-pointer">
-    //             <input
-    //               type="radio"
-    //               id="other"
-    //               value="2"
-    //               className="mr-1"
-    //               name="gender"
-    //               // {...register("gender", loginOptions.gender)}
-    //             />
-    //             <label htmlFor="other">Other</label>
-    //           </div>
-    //         </div>
-    //       </div>
-    //       <Button text="Save" fontsize="text-15" height="h-8" />
-    //     </form>
-    //   </div>
-    //   {/* choose avatar  */}
-    //   <div className="flex flex-col items-center mr-20">
-    //     <img
-    //       className="w-80 h-80 rounded-full mb-5"
-    //       src="https://images.unsplash.com/photo-1678626666675-d637950a4780?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1NHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
-    //       alt=""
-    //     />
-    //     <label className="border inline-block cursor-pointer px-2 py-1">
-    //       Change
-    //       <input type="file" className="hidden" />
-    //     </label>
-    //     <div className="mt-3">
-    //       <p className="text-15">
-    //         Maximum file size 1 MB <br /> Format: .JPEG, .PNG
-    //       </p>
-    //     </div>
-    //   </div>
-    // </div>
   );
 };
 
